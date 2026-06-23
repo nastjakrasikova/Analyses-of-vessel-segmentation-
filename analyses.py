a114ab073e20be0d1cd1ce24c830fb0c7d2530b2
nastjakrasikova <nastja.krasikova81@gmail.com>
2026-06-23 10:09:20 +0200

    Update analyses.py
    Co-authored-by: Stefano Moia <s.moia.research@gmail.com>

--- analyses.py
+++ analyses.py
@@ -12,7 +12,7 @@ def run_vascular_analysis():
     vessel_nii = nib.load("subject1_session1_refined_vessels.nii.gz")
     vessel_mask = vessel_nii.get_fdata()
 # load the Schaefer 2018 Local-Global Atlas
-atlas_nii = nib.load("Schaefer2018_LocalGlobal_Parcellation_MNI152_1mm.nii.gz")
+    atlas_nii = nib.load("Schaefer2018_LocalGlobal_Parcellation_MNI152_1mm.nii.gz")
     atlas_data = atlas_nii.get_fdata()
 
 
